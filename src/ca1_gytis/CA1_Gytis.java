59379913bf27f655edd31a46cc58e8968489eb08
GyVy <147094044+GyVys@users.noreply.github.com>
2024-01-12 20:33:16 +0000

    First commit

--- src/ca1_gytis/CA1_Gytis.java
+++ src/ca1_gytis/CA1_Gytis.java
@@ -4,6 +4,9 @@
  */
 package ca1_gytis;
 
+import java.io.FileReader;
+import java.util.Scanner;
+
 /**
  *
  * @author Gytis
@@ -15,6 +18,28 @@ public class CA1_Gytis {
      */
     public static void main(String[] args) {
         
+        int i = 1;
+        int j = 1;
+        String readLine;
+        
+        // reading info from file
+        try {
+            Scanner input = new Scanner(new FileReader("students.txt"));
+            
+        while (input.hasNextLine()) {
+            
+            
+            
+            String line = "Line" + i + " ";
+            
+            readLine = input.nextLine();
+            System.out.println(line + readLine);
+            i++;
+            }
+            
+        } catch (Exception e) {
+            System.out.println(e);
+        }
         
     }
     
